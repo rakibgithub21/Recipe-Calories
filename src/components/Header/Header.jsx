5915c6f2b02d5144cf49369268b86512a3abc9b9
rakibgithub21 <mdrakiulislam.gff@gmail.com>
2024-03-17 16:49:52 +0600

    last check

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,9 +1,11 @@
 
 import { VscThreeBars } from "react-icons/vsc";
-import logo from '../../assets/image-3.png'
+// import logo from '../../assets/image-3.png'
+import { FaRegUserCircle } from "react-icons/fa";
+
 
 const Header = () => {
-   
+
     return (
         <div className="container mx-auto py-10 ">
             <nav className="flex justify-between items-center">
@@ -19,7 +21,8 @@ const Header = () => {
                         <input type="text" className="grow " placeholder="Search" />
                         <svg xmlns="http://www.w3.org/2000/svg" viewBox="0 0 16 16" fill="currentColor" className="w-4 h-4 opacity-70"><path fillRule="evenodd" d="M9.965 11.026a5 5 0 1 1 1.06-1.06l2.755 2.754a.75.75 0 1 1-1.06 1.06l-2.755-2.754ZM10.5 7a3.5 3.5 0 1 1-7 0 3.5 3.5 0 0 1 7 0Z" clipRule="evenodd" /></svg>
                     </label>
-                    <img className="w-14 " src={logo} alt="" />
+                    {/* <img className="w-14 " src={logo} alt="" /> */}
+                    <p className="bg-[#0BE58A] p-2 rounded-full"><FaRegUserCircle className="text-4xl" /></p>
                 </div>
                 <div className="text-3xl lg:hidden">
                     <VscThreeBars />
